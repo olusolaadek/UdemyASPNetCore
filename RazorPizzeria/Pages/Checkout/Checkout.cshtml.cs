2378809d3202159a83da9cce0b6e88f6863cbfd5
OLUSOLA ADEKUNLE <olusola.adekunle@outlook.com>
2022-06-25 17:49:10 +0100

    Add DBContext and Persists data to the database using EF Core

--- RazorPizzeria/Pages/Checkout/Checkout.cshtml.cs
+++ RazorPizzeria/Pages/Checkout/Checkout.cshtml.cs
@@ -1,12 +1,38 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using RazorPizzeria.Data;
+using RazorPizzeria.Models;
 
 namespace RazorPizzeria.Pages.Checkout
 {
+    [BindProperties(SupportsGet = true)] // Binds all the properties and make them accessible to the view
     public class CheckoutModel : PageModel
     {
+        public string PizzaName { get; set; }
+        public float PizzaPrice { get; set; }
+        public string ImageTitle { get; set; }
+        private readonly ApplicationDbContext _context;
+        public CheckoutModel(ApplicationDbContext context)
+        {
+            _context = context;
+        }
         public void OnGet()
         {
+            if (String.IsNullOrWhiteSpace(PizzaName))
+            {
+                PizzaName = "Custom";
+            }
+            if (String.IsNullOrWhiteSpace(ImageTitle))
+            {
+                ImageTitle = "Create";
+            }
+            // var price = PizzaPrice;
+            PizzaOrder pizzaOrder = new PizzaOrder();
+            pizzaOrder.PizzaName = PizzaName;
+            pizzaOrder.BasePrice = PizzaPrice;
+
+            _context.PizzaOrders.Add(pizzaOrder);
+            _context.SaveChanges();
         }
     }
 }
--- RazorPizzeria/Pages/Forms/CustomPizza.cshtml.cs
+++ RazorPizzeria/Pages/Forms/CustomPizza.cshtml.cs
@@ -8,8 +8,25 @@ namespace RazorPizzeria.Pages.Forms
     {
         [BindProperty]
         public PizzasModel Pizza { get; set; }
+        public float PizzaPrice { get; set; }
         public void OnGet()
         {
+        }
+
+        public IActionResult OnPost()
+        {
+            PizzaPrice = Pizza.BasePrice;
+            if (Pizza.Beef) PizzaPrice += .25f;
+            if (Pizza.Tuna) PizzaPrice += .25f;
+            if (Pizza.Cheese) PizzaPrice += .50f;
+            if (Pizza.Ham) PizzaPrice += .5f;
+            if (Pizza.Mushroom) PizzaPrice += .50f;
+            if (Pizza.Peperoni) PizzaPrice += .50f;
+            if (Pizza.Pineapple) PizzaPrice += 10;
+            if (Pizza.TomatoSauce) PizzaPrice += .50f;
+
+            return RedirectToPage("/Checkout/Checkout", new { Pizza.PizzaName, PizzaPrice });
+            // return new OkResult();
         }
     }
 }
