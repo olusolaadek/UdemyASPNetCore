c5934a81921e502bbf091acaf4da3cee5936205d
OLUSOLA ADEKUNLE <olusola.adekunle@outlook.com>
2022-06-26 15:56:17 +0100

    Add model, scalfold model, and generate seeded data.

---
+++ MVCLibrary/Models/SeedData.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MVCLibrary.Data;
+
+namespace MVCLibrary.Models
+{
+    public class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new LibraryContext(
+                serviceProvider.GetRequiredService<DbContextOptions<LibraryContext>>()))
+            {
+                if (context.Book.Any())
+                {
+                    return;
+                }
+                context.AddRange(
+                    new Book { Title = "Tiny C# Projects", CallNumber = "TN-100021" },
+                    new Book { Title = "Tiny Android Projects", CallNumber = "TN-100022" }
+                    );
+                int result = context.SaveChanges();
+            };
+        }
+    }
+}
